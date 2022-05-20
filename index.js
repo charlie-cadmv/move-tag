3296054aad45ef197c6ef9c6cd72b6959659b66a
Charles Noble <Charles.Noble@dmv.ca.gov>
2022-05-20 14:35:06 -0700

    Format with Prettier

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
+const github = require("@actions/github");
+const core = require("@actions/core");
 
 async function run() {
-    // This should be a token with access to your repository scoped in as a secret.
-    // The YML workflow will need to set myToken with the GitHub Secret Token
-    // myToken: ${{ secrets.GITHUB_TOKEN }}
-    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    
-    const tagName = core.getInput('tag_name');
-    const commithash = core.getInput('commit_sha');
-    const myToken = core.getInput('token');
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
 
-    const octokit = github.getOctokit(myToken)
+  const tagName = core.getInput("tag_name");
+  const commithash = core.getInput("commit_sha");
+  const myToken = core.getInput("token");
 
-    // You can also pass in additional options as a second parameter to getOctokit
-    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+  const octokit = github.getOctokit(myToken);
 
-    try {
-                await github.git.deleteRef({
-                  owner: context.repo.owner,
-                  repo: context.repo.repo,
-                  ref: "tags/" + tagName
-                })
-            } catch (e) {
-              console.log("The nightly tag doesn't exist yet: " + e)
-            }
-            await github.git.createRef({
-              owner: context.repo.owner,
-              repo: context.repo.repo,
-              ref: "refs/tags/" + tagName,
-              sha: context.sha
-            })
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
 
-    console.log(pullRequest);
+  try {
+    await github.git.deleteRef({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: "tags/" + tagName,
+    });
+  } catch (e) {
+    console.log("The nightly tag doesn't exist yet: " + e);
+  }
+  await github.git.createRef({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    ref: "refs/tags/" + tagName,
+    sha: context.sha,
+  });
+
+  console.log(pullRequest);
 }
 
 run();
